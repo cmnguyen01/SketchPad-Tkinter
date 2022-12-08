cc85c396b36f4983ec9d46dcf9d389d204983978
cmnguyen01 <46499645+cmnguyen01@users.noreply.github.com>
2022-12-07 19:15:35 -0800

    made a mode class for each mode

--- sketchPad/sketchpad.py
+++ sketchPad/sketchpad.py
@@ -6,6 +6,8 @@ import numpy as np
 import tkinter as Tk
 from tkinter import * 
 from tkinter.ttk import * 
+from tkinter.filedialog import *
+from Mode import *
 from abc import ABC, abstractmethod
 class Shape:
     def __init__(self, master = None):
@@ -15,7 +17,7 @@ class Shape:
         self.create()
 
 
-class sketchpad:
+class sketchpad():
     
     def __init__(self,master=None):
         
@@ -29,14 +31,14 @@ class sketchpad:
         menubar = Menu(root)
         filemenu = Menu(menubar, tearoff=0)
         filemenu.add_command(label="New", command=self.donothing)
-        filemenu.add_command(label="Open", command=self.donothing)
-        filemenu.add_command(label="Save", command=self.donothing)
-        filemenu.add_command(label="Save as...", command=self.donothing)
+        filemenu.add_command(label="Open", command=self.open)
+        filemenu.add_command(label="Save", command=self.save_as)
+        filemenu.add_command(label="Save as...", command=self.save_as)
         filemenu.add_command(label="Close", command=root.destroy)
         menubar.add_cascade(label="File", menu=filemenu)
         editmenu = Menu(menubar, tearoff=0)
         editmenu.add_command(label="Undo", command=self.donothing)
-
+        
         editmenu.add_separator()
 
         editmenu.add_command(label="Cut", command=self.donothing)
@@ -58,26 +60,20 @@ class sketchpad:
         handButton.pack()
         graphButton=Button(root, text="Hand")
         graphButton.pack()
+        #create canvas
+        maincanvas=Canvas(root, bg="white",height=400, width=500)
+        maincanvas.pack()
         
-        
-      
+    #place holder function
     def donothing(self):
         filewin = Toplevel(self.root)
-    def save(self):
-        pass
+    def save_as(self):
+        f = asksaveasfile(initialfile = 'Untitled.jpeg',
+        defaultextension=".jpeg",filetypes=[("All Files","*.*"),("Text Documents","*.txt")])
+    def open(self):
+        f=askopenfile()
 
-class Mode(ABC):
-    def __init__(self) -> None:
-        pass
-class shapeMode(Mode):
-    def __init__(self) -> None:
-        pass
-class graphMode(Mode):
-    def __init__(self)-> None:
-        pass
-class handMode(Mode):
-    def __init__(self):
-        pass
+
 
 if __name__ == "__main__":
     #since the gui is the sketchpad i made the main open the sketchpad
