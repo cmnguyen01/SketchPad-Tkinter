4f44cae2a8529050fe9ad22ba0a4dafb3bbdec23
cmnguyen01 <46499645+cmnguyen01@users.noreply.github.com>
2023-01-19 19:23:36 -0800

    commit to git

--- sketchPad/Mode.py
+++ sketchPad/Mode.py
@@ -1,5 +1,10 @@
-
-
-class Mode():
-    def __init__(self) -> None:
+import networkx as nx
+import tkinter as Tk
+from tkinter import *
+from main import *
+class mode():
+    def __init__(self,master=None):
+        master=master
+        w.unbind("<B1-Motion>",paint)
+    def draw():
         pass
--- sketchPad/sketchpad.py
+++ sketchPad/sketchpad.py
@@ -4,96 +4,19 @@ import math
 import random
 import numpy as np
 import tkinter as Tk
-from graphMode import graphMode
+#from graphMode import graphMode
 from tkinter import * 
 from tkinter.ttk import * 
 from tkinter.filedialog import *
-
-from abc import ABC, abstractmethod
-class Shape:
-    def __init__(self, master = None):
-        self.master = master
-         
-        # Calls create method of class Shape
-        self.create()
-
-
-class sketchpad():
-    def paint(self,event):
-        x1, y1 = (event.x - 1), (event.y - 1)
-        x2, y2 = (event.x + 1), (event.y + 1)
-        w.create_oval(x1,y1,x2,y2,fill="black", width=0)
+from mode import *
+from main import *
+class sketchpad(mode):
     def createCircles(self,event):
         x1, y1 = (event.x - 1), (event.y - 1)
         x2, y2 = (event.x + 1), (event.y + 1)
-        w.create_oval(x1,y1,x2,y2,fill='red', width=5)
-    
+        w.create_oval(x1,y1,x2,y2,fill="red", width=5)
+
     def __init__(self,master=None):
+        master=master
+        w.unbind("<B1-Motion>",paint)
         
-        self.master=master
-        root=Tk()
-        root.title("Sketchpad")
-        root.geometry("500x500")
-        global w
-        
-        #menu
-        menubar = Menu(root)
-        filemenu = Menu(menubar, tearoff=0)
-        filemenu.add_command(label="New", command=self.new)
-        filemenu.add_command(label="Open", command=self.open)
-        filemenu.add_command(label="Save", command=self.save_as)
-        filemenu.add_command(label="Save as...", command=self.save_as)
-        filemenu.add_command(label="Close", command=root.destroy)
-        menubar.add_cascade(label="File", menu=filemenu)
-        editmenu = Menu(menubar, tearoff=0)
-        editmenu.add_command(label="Undo", command=self.donothing)
-        
-        editmenu.add_separator()
-
-        editmenu.add_command(label="Cut", command=self.donothing)
-        editmenu.add_command(label="Copy", command=self.donothing)
-        editmenu.add_command(label="Paste", command=self.donothing)
-        editmenu.add_command(label="Delete", command=self.donothing)
-        editmenu.add_command(label="Select All", command=self.donothing)
-
-        menubar.add_cascade(label="Edit", menu=editmenu)
-        helpmenu = Menu(menubar, tearoff=0)
-        helpmenu.add_command(label="Help Index", command=self.donothing)
-        helpmenu.add_command(label="About...", command=self.donothing)
-        menubar.add_cascade(label="Help", menu=helpmenu)
-        root.config(menu=menubar)
-        #buttons
-        graphButton=Button(root, text="Graph")
-        graphButton.bind("<Button-1>", self.graphButton_Press)
-        graphButton.pack()
-        #canvas
-        w=Canvas(root, bg="white",height=400, width=500)
-        w.pack(expand=YES, fill=BOTH)
-        w.bind("<B1-Motion>",self.paint)
-    #place holder function
-    def donothing(self):
-        filewin = Toplevel(self.root)
-    def save_as(self):
-        f = asksaveasfile(initialfile = 'Untitled.jpeg',
-        defaultextension=".jpeg",filetypes=[("All Files","*.*"),("jpeg","*.jpeg")])
-    def open(self):
-        f=askopenfile()
-    def new(self):
-        w.delete('all')
-    def graphButton_Press(self,event):
-        w.delete('all')
-        w.bind("<ButtonRelease>", self.createCircles)
-        w.unbind("<B1-motion>")
-        master=graphMode()
-        
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    #since the gui is the sketchpad i made the main open the sketchpad
-    master=sketchpad()
-    mainloop()
